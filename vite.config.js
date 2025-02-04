cc67e91ec0acbcb3d58448d96a50d1163bb43d9f
Conner Ow <conner@connerow.dev>
2025-02-04 08:02:44 -0600

    pages structure, startup animation

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        showcase: resolve(__dirname, 'showcase.html'),
+        brandkit: resolve(__dirname, 'brandkit.html'),
+      },
+    },
+  },
+})
