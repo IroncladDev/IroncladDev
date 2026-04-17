438c14c2ae4ffe3236d49640c9fc88bca3308fba
IroncladDev <conner@ironclad.sh>
2026-04-17 14:02:11 -0500

    fix: compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  redirects: {
-    '/signal':
-      'https://signal.me/#eu/36E3b48gh-bGeLWnxgNERMFW4315Pdtjmu3-vcqKZ8Bs0WSY-HpEGZ-qmGgAa5gV',
-    '/chat':
-      'https://signal.group/#CjQKIFso0Z5zIUiTAcueMbLgezhczQ19cO-STQK6LZfDbKzqEhAR2_vENgBwvx1qdTHpDnR-',
-  },
+    vite: {
+        server: {
+            allowedHosts: ['local-host.live'],
+        },
+        preview: {
+            allowedHosts: ['local-host.live'],
+        },
+    },
 })
