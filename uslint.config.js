7ac14e8201d124ead02fd1f1b31f5a843aecb194
IroncladDev <conner@ironclad.sh>
2026-06-18 18:55:35 -0500

    fix: remove layout shift for initial page load

--- uslint.config.js
+++
@@ -1,8 +0,0 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-]);
